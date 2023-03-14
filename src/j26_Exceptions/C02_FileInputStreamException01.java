9564039b3a38dca0b37b3087dbf0dc82f48bbb67
Theyusuf41 <yusufakrglu@gmail.com>
2023-03-15 00:09:49 +0100

    Exceptions

---
+++ src/j26_Exceptions/C02_FileInputStreamException01.java
@@ -0,0 +1,18 @@
+package j26_Exceptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C02_FileInputStreamException01 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis= new FileInputStream("C:\\Users\\yusuf\\IdeaProjects\\working\\src\\j26_Exceptions\\random");
+        // fis path verilen path'e erisim saglar
+        // FileNotFoundException -> yolu belirlenen file path yanlissa durumu kurtarmak icin kullanilan yontem
+        int k;
+        while((k= fis.read()) !=-1) { // fis.ready dosyadakileri okumak icindir bu nedenle yeni bir exception throw etmesi gerekir buda -> IOException'dir
+            System.out.print((char) k);
+
+        }
+    }
+}
---
+++ src/j26_Exceptions/C03_ClassCastException.java
@@ -0,0 +1,31 @@
+package j26_Exceptions;
+
+public class C03_ClassCastException {
+    public static void main(String[] args) {
+        // Birbirine convert edilemeyen data typelar donusturme isleminde olusan RTException
+        Object obj = "Herkese selamlar";
+        String str = (String) obj; // object data type String data type'a cevirildi
+        System.out.println(str);
+
+
+        Object sayi =33;
+        //String str1 = (String) sayi;
+        // System.out.println(str1); // Exception verdi
+        try{
+            String str1 = (String) sayi;
+            System.out.println("Basari ile calisti (try)");
+        }
+        catch (ClassCastException e) {
+            System.out.println("Basari ile calisti (catch)");
+        }
+        System.out.println("Basari ile calisti!!!");
+
+        try{
+            String str2 = (String) obj;System.out.println("Basari ile calisti (try)");
+        }
+        catch (ClassCastException e){
+            System.out.println("Basari ile calisti (catch)");
+        }
+        System.out.println("Basari ile calisti???");
+    }
+}
